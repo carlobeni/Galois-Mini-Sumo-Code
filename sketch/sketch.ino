a07bde47f694cce9e701e54bfd5305ffa40a6933
carlobeni <carlosbenitez.dev.acct@gmail.com>
2024-07-03 17:52:30 -0400

    +WIFI OTA

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -4,6 +4,11 @@
 #include "GestorEstadosAlpha.h"
 #include <IRremote.h>
 
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
 const int RECV_PIN = 13;
 IRrecv irrecv(RECV_PIN);
 decode_results results;
@@ -67,8 +72,62 @@ DetectorLinea detL(pinSensoresL, numSensoresL);
 
 GestorEstadosAlpha gestorEstados(controlMov, detE, detL);
 
+const char* ssid    = "PROFESORES";
+const char* password = "profeFIUNA#2024";
+
+void setupOTA(){
+  Serial.println("Cargando WIFI...");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Error con Wifi, reiniciando...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  // Configuración de OTA
+  ArduinoOTA.setHostname("GaloisCodeWIFI");
+  ArduinoOTA.setPassword("2357");
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else {  // U_FS
+      type = "filesystem";
+    }
+    Serial.println("Iniciando Programación " + type);
+  });
+
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nTerminando");
+  });
+
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progreso: %u%%\r", (progress / (total / 100)));
+  });
+
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+  Serial.println("Listo");
+  Serial.print("Dirección IP: ");
+  Serial.println(WiFi.localIP());
+}
 void setup()
 {
+  setupOTA();
   Serial.begin(115200);
   detE.begin();
   detL.begin();
@@ -79,6 +138,7 @@ void setup()
 
 void loop()
 {
+  ArduinoOTA.handle();
   if (irrecv.decode(&results))
   {
     if (results.decode_type == SONY)
---
+++ wifi_setting/wifi_setting.ino
@@ -0,0 +1,69 @@
+
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+const char* ssid    = "PROFESORES";
+const char* password = "profeFIUNA#2024";
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Cargando sistema...");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Error con Wifi, reiniciando...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  // Puerto de programacion, defecto 3232
+  // ArduinoOTA.setPort(3232);
+
+  // Nombre en Red, defecto esp3232-[MAC]
+  ArduinoOTA.setHostname("GaloisCodeWIFI");
+  ArduinoOTA.setPassword("2357");
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+  
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else {  // U_FS
+      type = "filesystem";
+    }
+
+    Serial.println("Iniciando Programación " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nTerminando");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progreso: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+
+  ArduinoOTA.begin(); 
+
+  Serial.println("Listo");
+  Serial.print("Dirección IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  ArduinoOTA.handle();
+
+}
